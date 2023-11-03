da643ea6bd69ca872bd8a0d19defe7dfaa9302d4
Mahdi sadeghi <mahdi.sadeghi1915@gmail.com>
2023-11-03 13:01:04 +0330

    hash user password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Controller, Post,Body } from '@nestjs/common';
+import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+ 
+    constructor(private authService:AuthService){}
+
+    @Post('/signup')
+    signUp(@Body() authCredentialsDto:AuthCredentialsDto):Promise<void>{
+        return this.authService.singUp(authCredentialsDto);
+    }
+
+
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './users.repository';
@@ -7,4 +8,9 @@ export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
         private userRepository:UserRepository,){}
+
+
+    async singUp(authCredentialsDto:AuthCredentialsDto):Promise<void>{
+        return this.userRepository.createUser(authCredentialsDto);
+    }
 }
